23bc75b4e0e8b27637b4af837f9eb803b291752e
Bill Garr <bgarr.thinkabt@gmail.com>
2024-06-14 18:06:40 -0400

    change welcome message

--- app/javascript/src/entrypoints/application.tsx
+++ app/javascript/src/entrypoints/application.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-export const App = () => <h1>Plan My Escape!</h1>;
+export const App = () => <h1>Hello from Plan My Escape!</h1>;
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = ReactDOM.createRoot(
