ffd4793ab380fe64781b11cf5c3db8b4819527f4
Bill Garr <bgarr.thinkabt@gmail.com>
2024-06-14 17:55:27 -0400

    test precommit hook

--- app/javascript/src/entrypoints/application.tsx
+++ app/javascript/src/entrypoints/application.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-export const App = () => <h1>Hello from React!</h1>;
+export const App = () => <h1>Plan My Escape!</h1>;
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = ReactDOM.createRoot(
