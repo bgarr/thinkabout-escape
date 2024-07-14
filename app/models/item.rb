ac726229dbf335562a184361d47600ae54fe9e81
Bill Garr <bgarr.thinkabt@gmail.com>
2024-07-14 14:47:30 -0400

    remove :optional from item has_one puzzle

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class Item < ApplicationRecord
-  has_one :puzzle, optional: true, dependent: :nullify
+  has_one :puzzle, dependent: :nullify
   has_one_attached :model_file
 end
