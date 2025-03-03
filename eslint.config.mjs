13fd358b6144c74b5d058f5f709a0c034929de7a
Bill Garr <bgarr.thinkabt@gmail.com>
2025-03-02 19:07:53 -0500

    eslint ts works

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,29 +1,9 @@
 // @ts-check
 
-import js from "@eslint/js";
-import * as tsParser from "@typescript-eslint/parser";
-import globals from "globals";
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
 
-export default [
-  js.configs.recommended,
-  {
-    languageOptions: {
-      parserOptions: {
-        parser: tsParser,
-        project: "./tsconfig.json", // <-- Point to your project's "tsconfig.json" or create a new one.
-      },
-    },
-  },
-  {
-    files: ["**/*.{ts,tsx}"],
-  },
-  {
-    languageOptions: {
-      globals: {
-        ...globals.jest,
-        window: true,
-        document: true,
-      },
-    },
-  },
-];
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended
+);
