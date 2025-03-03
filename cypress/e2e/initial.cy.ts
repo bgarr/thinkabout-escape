c803ce6a05c48c8183607d125e03afe05151e12d
Bill Garr <bgarr.thinkabt@gmail.com>
2025-03-03 14:15:35 -0500

    unplug cypress test in github actions as well

--- cypress/e2e/initial.cy.ts
+++ cypress/e2e/initial.cy.ts
@@ -1,16 +1,16 @@
-import { cy } from "local-cypress"
+import { beforeEach, cy, describe, it } from "local-cypress";
 
-describe('start the app', () => {
+describe("start the app", () => {
   beforeEach(() => {
-    cy.visit('/')
-  })
+    cy.visit("/");
+  });
 
-  it('responds to user actions', () => {
+  it("responds to user actions", () => {
     // We use the `cy.get()` command to get all elements that match the selector.
     // Then, we use `should` to assert that there are two matched items,
     // which are the two default items.
-    const checkbox = cy.get('#the-check-box')
-    checkbox.check()
+    const checkbox = cy.get("#the-check-box");
+    checkbox.check();
     checkbox.should("be.checked");
-  })
-})
+  });
+});
