c955bab0632f7a7e06e677623e4058d1b82ee251
Bill Garr <bgarr.thinkabt@gmail.com>
2025-03-22 07:38:58 -0400

    rubocop fix

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -15,7 +15,8 @@ Rails.application.configure do
     policy.script_src :self, :https
     policy.style_src :self, :https, :unsafe_inline
     # Allow @vite/client to hot reload javascript changes in development
-    #    policy.script_src *policy.script_src, :unsafe_eval, "http://#{ ViteRuby.config.host_with_port }" if Rails.env.development?
+    #    policy.script_src *policy.script_src, :unsafe_eval,
+    #        "http://#{ ViteRuby.config.host_with_port }" if Rails.env.development?
 
     # You may need to enable this in production as well depending on your setup.
     #    policy.script_src *policy.script_src, :blob if Rails.env.test?
